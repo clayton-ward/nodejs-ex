b612e4c92d0f5836ba8de8cde472ee81572a9ee0
Clayton Ward <clward@CLWARD0315VM.local>
2017-09-29 13:40:07 -0500

    added host

--- server.js
+++ server.js
@@ -62,17 +62,17 @@ app.get('/', function (req, res) {
   if (!db) {
     initDb(function(err){});
   }
+  var os = require("os")
+  var hostname = os.hostname()
   if (db) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
-    var os = require("os")
-    var hostname = os.hostname()
     col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails, pod: hostname });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('index.html', { pageCountMessage : null, pod: hostname});
   }
 });
 
